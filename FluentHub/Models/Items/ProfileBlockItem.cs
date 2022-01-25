1ebb0caa07c340eb34d41c5bb7a4d3f04d4fa81d
NullPointerException <62196528+onein528@users.noreply.github.com>
2022-01-26 00:29:25 +0900

    Update the UI of the user profile page (#15)

---
+++ FluentHub/Models/Items/ProfileBlockItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace FluentHub.Models.Items
+{
+    public class ProfileBlockItem
+    {
+        public string Glygh { get; set; }
+        public StackPanel ElementContent { get; set; }
+    }
+}
--- FluentHub/Views/UserPages/ProfilePage.xaml.cs
+++ FluentHub/Views/UserPages/ProfilePage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -38,57 +39,9 @@ namespace FluentHub.Views.UserPages
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             user = await App.Client.User.Get(requestedUsername);
-            SetUserInfo();
-        }
 
-        private void SetUserInfo()
-        {
-            // Avator
-            BitmapImage avatorImage = new BitmapImage(new Uri(user.AvatarUrl));
-            UserAvatorImage.Source = avatorImage;
-
-            // Username
-            if (user.Login != "")
-            {
-                Username.Text = user.Login;
-            }
-
-            // Fullname
-            if (user.Name != "")
-            {
-                FullName.Text = user.Name;
-            }
-
-            // Bio
-            if (user.Bio != "")
-            {
-                UserBioTextBlock.Text = user.Bio;
-                UserBioBlock.Visibility = Visibility.Visible;
-            }
-
-            //Link
-            if (user.Blog != "")
-            {
-                LinkButton.Content = user.Blog;
-                var uri = new UriBuilder(user.Blog).Uri;
-                LinkButton.NavigateUri = uri;
-                LinkBlock.Visibility = Visibility.Visible;
-            }
-
-            // Location
-            if (user.Location != "")
-            {
-                LocationTextBlock.Text = user.Location;
-                LocationBlock.Visibility = Visibility.Visible;
-            }
-
-            // FF
-            FollowerCount.Text = user.Followers.ToString();
-            FollowingCount.Text = user.Following.ToString();
-        }
-
-        private void UserNavView_Loaded(object sender, RoutedEventArgs e)
-        {
+            _=await SetTopLevelUserInfo();
+            ViewModel.SetProfileElements(user);
         }
 
         private void UserNavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
@@ -112,18 +65,94 @@ namespace FluentHub.Views.UserPages
             }
         }
 
-        private void FollowersButton_Click(object sender, RoutedEventArgs e)
+        private async Task<bool> SetTopLevelUserInfo()
         {
-            UserNavView.SelectedItem = null;
+            // Avator
+            BitmapImage avatorImage = new BitmapImage(new Uri(user.AvatarUrl));
+            UserAvatorImage.Source = avatorImage;
 
-            UserNavViewContent.Navigate(typeof(Followers));
-        }
+            // Username
+            if (user.Login != "")
+            {
+                Username.Text = user.Login;
+            }
 
-        private void FollowingButton_Click(object sender, RoutedEventArgs e)
-        {
-            UserNavView.SelectedItem = null;
+            // Fullname
+            if (user.Name != "")
+            {
+                FullName.Text = user.Name;
+            }
 
-            UserNavViewContent.Navigate(typeof(Following));
+            // Company
+            if (user.Company != "")
+            {
+                try
+                {
+                    if (user.Company.IndexOf("@") == 0)
+                    {
+                        CompanyLinkButton.Content = user.Company;
+                        string company = user.Company.Replace("@", "");
+
+                        var userCompany = await App.Client.User.Get(company);
+                        var org = await App.Client.Organization.Get(company);
+                        string navigateUrl = null;
+                        navigateUrl = App.DefaultHost + "/" + company;
+                        var uri = new UriBuilder(navigateUrl).Uri;
+                        CompanyLinkButton.NavigateUri = uri;
+                        CompanyLinkButton.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        CompanyLinkTextBlock.Text = user.Company;
+                        CompanyLinkTextBlock.Visibility = Visibility.Visible;
+                    }
+                }
+                catch
+                {
+                    CompanyLinkTextBlock.Text = user.Company;
+                    CompanyLinkTextBlock.Visibility = Visibility.Visible;
+                }
+
+                CompanyBlock.Visibility = Visibility.Visible;
+            }
+
+            // Bio
+            if (user.Bio != "")
+            {
+                UserBioTextBlock.Text = user.Bio;
+                UserBioTextBlock.Visibility = Visibility.Visible;
+            }
+
+            //Link
+            if (user.Blog != "")
+            {
+                LinkButton.Content = user.Blog;
+                var uri = new UriBuilder(user.Blog).Uri;
+                LinkButton.NavigateUri = uri;
+                LinkBlock.Visibility = Visibility.Visible;
+            }
+
+            // Location
+            if (user.Location != "")
+            {
+                LocationTextBlock.Text = user.Location;
+                LocationBlock.Visibility = Visibility.Visible;
+            }
+
+            // FF
+            FollowerCount.Text = user.Followers.ToString();
+            FollowingCount.Text = user.Following.ToString();
+
+            if (user.Login == App.SignedInUserName)
+            {
+                EditProfileButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                FollowButton.Visibility = Visibility.Visible;
+            }
+
+            return true;
         }
     }
 }
