03cd46db749f6eec7a87c195b7cd7f864a147484
NullPointerException <onein528@outlook.com>
2022-01-31 23:40:42 +0900

    Update repo pages

--- FluentHub/Views/RepoPages/OverviewPage.xaml.cs
+++ FluentHub/Views/RepoPages/OverviewPage.xaml.cs
@@ -18,6 +18,8 @@ namespace FluentHub.Views.RepoPages
 {
     public sealed partial class OverviewPage : Page
     {
+        private long RepoId { get; set; }
+
         public OverviewPage()
         {
             this.InitializeComponent();
@@ -25,7 +27,35 @@ namespace FluentHub.Views.RepoPages
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            var RepoId = e.Parameter as string;
+            RepoId = Convert.ToInt64(e.Parameter as string);
+        }
+
+        private void RepoPageNavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            switch (args.SelectedItemContainer.Tag.ToString())
+            {
+                case "Code":
+                    RepoPageNavViewFrame.Navigate(typeof(CodePage));
+                    break;
+                case "Issues":
+                    RepoPageNavViewFrame.Navigate(typeof(IssueListPage));
+                    break;
+                case "PRs":
+                    RepoPageNavViewFrame.Navigate(typeof(PullRequestListPage));
+                    break;
+                case "Settings":
+                    RepoPageNavViewFrame.Navigate(typeof(Settings));
+                    break;
+            }
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var repo = await App.Client.Repository.Get(RepoId);
+
+            RepoOwnerName.Text = repo.Owner.Login;
+
+            RepoName.Text = repo.Name;
         }
     }
 }
