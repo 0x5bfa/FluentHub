7cb25256c4f922635f5476df3bbb21bd07300458
NullPointerException <onein528@outlook.com>
2022-02-03 01:16:03 +0900

    Get repository root contents

---
+++ FluentHub/ViewModels/RepoPages/CodePageViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace FluentHub.ViewModels.RepoPages
+{
+    public class DetailsLayoutModel
+    {
+        public string ObjectTypeIconGlyph { get; set; }
+
+        public string ObjectName { get; set; }
+
+        public string ObjectLatestCommitMessage { get; set; }
+
+        public string ObjectUpdatedAtHumanized { get; set; }
+    }
+
+    public class CodePageViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<DetailsLayoutModel> _items = new ObservableCollection<DetailsLayoutModel>();
+        public ObservableCollection<DetailsLayoutModel> Items
+        {
+            get => _items;
+            private set
+            {
+                _items = value;
+                NotifyPropertyChanged(nameof(Items));
+            }
+        }
+
+        public async Task EnumRepositoryContents(long repoId)
+        {
+            var contents = await App.Client.Repository.Content.GetAllContents(repoId);
+
+            foreach(var item in contents)
+            {
+                DetailsLayoutModel listItem = new DetailsLayoutModel();
+
+                if (item.Type == Octokit.ContentType.Dir)
+                {
+                    listItem.ObjectTypeIconGlyph = "\uE9A0";
+                }
+                else
+                {
+                    listItem.ObjectTypeIconGlyph = "\uE996";
+                }
+
+                listItem.ObjectName = item.Name;
+
+                Octokit.CommitRequest request = new Octokit.CommitRequest();
+                request.Path = item.Path;
+
+                var commit = await App.Client.Repository.Commit.GetAll(repoId, request);
+
+                listItem.ObjectLatestCommitMessage = commit[0].Commit.Message.Split("\n")[0];
+
+                listItem.ObjectUpdatedAtHumanized = commit[0].Commit.Author.Date.Humanize();
+
+                Items.Add(listItem);
+            }
+
+
+            Items = new ObservableCollection<DetailsLayoutModel>(Items.OrderByDescending(x => x.ObjectTypeIconGlyph));
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- FluentHub/Views/RepoPages/CodePage.xaml.cs
+++ FluentHub/Views/RepoPages/CodePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Humanizer;
+using FluentHub.Services.OctokitEx;
+using Octokit;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,14 +14,17 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 
 namespace FluentHub.Views.RepoPages
 {
-    public sealed partial class CodePage : Page
+    public sealed partial class CodePage : Windows.UI.Xaml.Controls.Page
     {
         private long RepoId { get; set; }
+        private Repository Repository { get; set; }
+        private Readme Readme { get; set; }
 
         public CodePage()
         {
@@ -44,9 +50,68 @@ namespace FluentHub.Views.RepoPages
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            var repo = await App.Client.Repository.Get(RepoId);
+            Repository = await App.Client.Repository.Get(RepoId);
 
-            RepoDescription.Text = repo.Description;
+            RepoDescription.Text = Repository.Description;
+
+            if (Readme != null)
+            {
+                OverviewReadmeBlock.Visibility = Visibility.Visible;
+            }
+
+            if (Repository.License != null)
+            {
+                OverviewLicenseBlock.Visibility = Visibility.Visible;
+                OverviewLicense.Content = Repository.License.Name;
+            }
+
+            OverviewStargazersCount.Content = Repository.StargazersCount.ToString() + " Stars";
+
+            OverviewWatchingCount.Content = Repository.SubscribersCount.ToString() + " Watching";
+
+            OverviewForksCount.Content = Repository.ForksCount.ToString() + " Forks";
+
+            var commits = await App.Client.Repository.Commit.GetAll(RepoId);
+
+
+            RepoLatestCommitAuthorAvatar.Source = new BitmapImage(new Uri(commits[0].Author.AvatarUrl));
+
+            RepoLatestCommitAuthorName.Text = commits[0].Author.Login;
+
+            RepoLatestCommitSha.Text = commits[0].Sha.Substring(0, 7);
+
+            RepoLatestCommitUpdatedAtHumanized.Text = commits[0].Commit.Author.Date.Humanize();
+
+            RepoCommitsCount.Text = commits.Count().ToString();
+
+            await ViewModel.EnumRepositoryContents(RepoId);
+        }
+
+        private void RepositoryReadmeWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            if (args.Uri != null)
+            {
+                args.Cancel = true;
+            }
+        }
+
+        private async void RepositoryReadmeWebView_Loaded(object sender, RoutedEventArgs e)
+        {
+            Readme = await App.Client.Repository.Content.GetReadme(RepoId);
+            Markdown markdown = new Markdown();
+
+            if (Readme == null)
+            {
+                return;
+            }
+            else
+            {
+                RepositoryReadmeBlock.Visibility = Visibility.Visible;
+            }
+
+            string result = await markdown.FormatRenderedMarkdownToHtml(await Readme.GetHtmlContent());
+
+            RepositoryReadmeWebView.NavigateToString(result);
         }
     }
 }
