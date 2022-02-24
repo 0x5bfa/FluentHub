b37b08f04efd68793f999719dd11993ca6579df7
onein528 <onein528@outlook.com>
2022-02-25 01:18:47 +0900

    update label control

--- FluentHub/UserControls/Labels/LabelControl.xaml.cs
+++ FluentHub/UserControls/Labels/LabelControl.xaml.cs
@@ -20,97 +20,76 @@ namespace FluentHub.UserControls.Labels
 {
     public sealed partial class LabelControl : UserControl
     {
-        #region AccentColorProperty
-        public static readonly DependencyProperty AccentColorProperty
-        = DependencyProperty.Register(
-              nameof(LabelControl),
-              typeof(Brush),
-              typeof(LabelControl),
-              new PropertyMetadata(null)
-            );
+        public static readonly DependencyProperty IconProperty
+            = DependencyProperty.Register(nameof(Icon), typeof(IconElement), typeof(LabelControl), new PropertyMetadata(null));
 
-        public Brush AccentColor
+        public IconElement Icon
         {
-            get => (Brush)GetValue(AccentColorProperty);
-            set => SetValue(AccentColorProperty, value);
+            get => (IconElement)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
         }
-        #endregion
 
-        #region FontGlyphProperty
-        public static readonly DependencyProperty FontGlyphProperty
-            = DependencyProperty.Register(
-                  nameof(FontGlyph),
-                  typeof(string),
-                  typeof(LabelControl),
-                  new PropertyMetadata(null)
-                );
-
-        public string FontGlyph
-        {
-            get => (string)GetValue(FontGlyphProperty);
-            set => SetValue(FontGlyphProperty, value);
-        }
-        #endregion
-
-        #region LabelTextProperty
         public static readonly DependencyProperty LabelTextProperty
-            = DependencyProperty.Register(
-                  nameof(LabelText),
-                  typeof(string),
-                  typeof(LabelControl),
-                  new PropertyMetadata(null)
-                );
+            = DependencyProperty.Register(nameof(LabelText), typeof(string), typeof(LabelControl), new PropertyMetadata(null));
 
         public string LabelText
         {
             get => (string)GetValue(LabelTextProperty);
             set => SetValue(LabelTextProperty, value);
         }
-        #endregion
+
+        public static readonly DependencyProperty LabelBackgroundProperty
+            = DependencyProperty.Register(nameof(LabelBackground), typeof(Brush), typeof(LabelControl), new PropertyMetadata(null));
+
+        public Brush LabelBackground
+        {
+            get => (Brush)GetValue(LabelBackgroundProperty);
+            set => SetValue(LabelBackgroundProperty, value);
+        }
 
         public LabelControl()
         {
             this.InitializeComponent();
         }
 
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            LabelBackground.Background = AccentColor;
+        //private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    LabelBackground.Background = AccentColor;
 
-            //Color color = ((SolidColorBrush)AccentColor).Color;
+        //    //Color color = ((SolidColorBrush)AccentColor).Color;
 
-            //float H = 0; float S = 0; float L = 0;
-            //ColorHelpers.RgbToHsl(color.R, color.G, color.B, out H, out S, out L);
+        //    //float H = 0; float S = 0; float L = 0;
+        //    //ColorHelpers.RgbToHsl(color.R, color.G, color.B, out H, out S, out L);
 
-            //SetColorAdjustedBorderAndColor(color.R, color.G, color.B, H, S, L);
+        //    //SetColorAdjustedBorderAndColor(color.R, color.G, color.B, H, S, L);
 
-            //LabelBorder.BorderBrush = AccentColor;
-            //LabelTextBlock.Foreground = AccentColor;
+        //    //LabelBorder.BorderBrush = AccentColor;
+        //    //LabelTextBlock.Foreground = AccentColor;
 
-            LabelTextBlock.Text = LabelText;
-        }
+        //    LabelTextBlock.Text = LabelText;
+        //}
 
-        private void SetColorAdjustedBorderAndColor(int R, int G, int B, float H, float S, float L)
-        {
-            // This method was created from github web-site style named flash.scss
-            // but I could not update color blightness correctly. I don't know why.
-            float lightnessThreshold = 0.6F;
+        //private void SetColorAdjustedBorderAndColor(int R, int G, int B, float H, float S, float L)
+        //{
+        //    // This method was created from github web-site style named flash.scss
+        //    // but I could not update color blightness correctly. I don't know why.
+        //    float lightnessThreshold = 0.6F;
 
-            float perceivedLightness = ((R * 0.2126F) + (G * 0.7152F) + (B * 0.0722F)) / 255F;
-            float lightnessSwitch = (perceivedLightness - lightnessThreshold) * -1000;
-            lightnessSwitch = ((lightnessSwitch < 1) ? lightnessSwitch : 1);
-            lightnessSwitch =( (lightnessSwitch < 0) ? 0 : lightnessSwitch);
+        //    float perceivedLightness = ((R * 0.2126F) + (G * 0.7152F) + (B * 0.0722F)) / 255F;
+        //    float lightnessSwitch = (perceivedLightness - lightnessThreshold) * -1000;
+        //    lightnessSwitch = ((lightnessSwitch < 1) ? lightnessSwitch : 1);
+        //    lightnessSwitch =( (lightnessSwitch < 0) ? 0 : lightnessSwitch);
 
-            float lightenBy = ((lightnessThreshold - perceivedLightness) * 100) * lightnessSwitch;
+        //    float lightenBy = ((lightnessThreshold - perceivedLightness) * 100) * lightnessSwitch;
 
-            S *= 0.01F;
-            L = (L + lightenBy) * 0.01F;
+        //    S *= 0.01F;
+        //    L = (L + lightenBy) * 0.01F;
 
-            ColorHelpers.HslToRgb(H, S, L, out R, out G, out B);
+        //    ColorHelpers.HslToRgb(H, S, L, out R, out G, out B);
 
-            var adjustedBrush = new SolidColorBrush(Color.FromArgb(0xFF, (byte)R, (byte)G, (byte)B));
-            LabelBorder.BorderBrush = adjustedBrush;
-            LabelTextBlock.Foreground = adjustedBrush;
-        }
+        //    var adjustedBrush = new SolidColorBrush(Color.FromArgb(0xFF, (byte)R, (byte)G, (byte)B));
+        //    LabelBorder.BorderBrush = adjustedBrush;
+        //    LabelTextBlock.Foreground = adjustedBrush;
+        //}
     }
 }
