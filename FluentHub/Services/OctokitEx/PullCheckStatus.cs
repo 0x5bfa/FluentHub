63c588bf1cda7c1875da4c8bae380aaadeac7564
onein528 <onein528@outlook.com>
2022-02-26 01:52:59 +0900

    prepare to get check and review status

---
+++ FluentHub/Services/OctokitEx/PullCheckStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentHub.Services.OctokitEx
+{
+    public class PullCheckStatus
+    {
+        public bool GetLatestCheckStatus(int repositoryId, int number)
+        {
+            return false;
+        }
+    }
+}
---
+++ FluentHub/Services/OctokitEx/PullReviewStatus.cs
@@ -0,0 +1,48 @@
+﻿using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentHub.Services.OctokitEx
+{
+    public class PullReviewStatus
+    {
+        public bool GetLatestReviewStatus(string owner, string name, int number)
+        {
+            try
+            {
+                /*
+                string token = App.Settings.Get("AccessToken", "");
+
+                var graphQLClient = new GraphQLHttpClient($"{App.GraphQlApiEndPoint}", new NewtonsoftJsonSerializer());
+
+                graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", $"bearer {token}");
+
+                string query = null;
+
+                List<long> repoIdList = new List<long>();
+
+                query = "query { user(login: \"" + username + "\") {pinnedItems(first: 6) {nodes {... on Repository {nameWithOwner}}}}}";
+                var repositoriesRequest = new GraphQLRequest { Query = query };
+
+                var graphQLResponse = await graphQLClient.SendQueryAsync<UserResponseType>(repositoriesRequest);
+
+                foreach (var fullname in graphQLResponse.Data.User.PinnedItems.Nodes)
+                {
+                    var repoId = await GetRepoIdFromRepoNameAndOwner(fullname.NameWithOwner);
+                    repoIdList.Add(repoId);
+                }
+                */
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
