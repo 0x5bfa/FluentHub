9e5ca55ccf6df4799043af65ad6f44e07b2aff1f
onein528 <onein528@outlook.com>
2022-03-16 00:41:43 +0900

    update viewer's notification page

---
+++ src/FluentHub/UserControls/ButtonBlocks/NotificationButtonBlock.xaml.cs
@@ -0,0 +1,47 @@
+﻿using FluentHub.Models.Items;
+using FluentHub.ViewModels.UserControls.ButtonBlocks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace FluentHub.UserControls.ButtonBlocks
+{
+    public sealed partial class NotificationButtonBlock : UserControl
+    {
+        #region dprops
+        public static readonly DependencyProperty ViewModelProperty
+            = DependencyProperty.Register(
+                  nameof(NotificationListItem),
+                  typeof(NotificationButtonBlockViewModel),
+                  typeof(NotificationButtonBlock),
+                  new PropertyMetadata(null)
+                );
+
+        public NotificationButtonBlockViewModel ViewModel
+        {
+            get => (NotificationButtonBlockViewModel)GetValue(ViewModelProperty);
+            set
+            {
+                SetValue(ViewModelProperty, value);
+                this.DataContext = ViewModel;
+            }
+        }
+        #endregion
+
+        public NotificationButtonBlock()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/FluentHub/ViewModels/UserControls/ButtonBlocks/NotificationButtonBlockViewModel.cs
@@ -0,0 +1,36 @@
+﻿using FluentHub.Models.Items;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentHub.ViewModels.UserControls.ButtonBlocks
+{
+    public class NotificationButtonBlockViewModel : INotifyPropertyChanged
+    {
+        public NotificationListItem NotificationItem { get; set; } = new();
+
+        private string nameWithOwner;
+        public string NameWithOwner { get => nameWithOwner; set => SetProperty(ref nameWithOwner, value); }
+
+        private string updatedAtHumanized;
+        public string UpdatedAtHumanized { get => updatedAtHumanized; set => SetProperty(ref updatedAtHumanized, value); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
