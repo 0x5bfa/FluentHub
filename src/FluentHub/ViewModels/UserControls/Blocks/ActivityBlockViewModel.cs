a2311da774f5fc22165790068b51e0a4529095e1
onein528 <onein528@outlook.com>
2022-03-22 23:51:46 +0900

    update

--- src/FluentHub/ViewModels/UserControls/Blocks/ActivityBlockViewModel.cs
+++ src/FluentHub/ViewModels/UserControls/Blocks/ActivityBlockViewModel.cs
@@ -22,8 +22,42 @@ namespace FluentHub.ViewModels.UserControls.Blocks
         private string updatedAtHumanized;
         public string UpdatedAtHumanized { get => updatedAtHumanized; set => SetProperty(ref updatedAtHumanized, value); }
 
+        private RepoButtonBlockViewModel repoBlockViewModel;
+        public RepoButtonBlockViewModel RepoBlockViewModel { get => repoBlockViewModel; set => SetProperty(ref repoBlockViewModel, value); }
+
+        private UserButtonBlockViewModel userBlockViewModel;
+        public UserButtonBlockViewModel UserBlockViewModel { get => userBlockViewModel; set => SetProperty(ref userBlockViewModel, value); }
+
+        private CommitActivityBlockViewModel commitBlockViewModel;
+        public CommitActivityBlockViewModel CommitBlockViewModel { get => commitBlockViewModel; set => SetProperty(ref commitBlockViewModel, value); }
+
         public async Task GetPayloadContents()
         {
+            if (Payload.IsForkEvent)
+            {
+                RepoBlockViewModel = new();
+                RepoBlockViewModel.DisplayDetails = false;
+                RepoBlockViewModel.DisplayStarButton = true;
+
+                RepositoryQueries queries = new();
+                var repo = await queries.GetOverview(Payload.Repository.Name.Split("/")[1], Payload.Repository.Name.Split("/")[0]);
+                RepoBlockViewModel.Item = repo;
+            }
+            else if (Payload.IsWatchEvent)
+            {
+                RepoBlockViewModel = new();
+                RepoBlockViewModel.DisplayDetails = false;
+                RepoBlockViewModel.DisplayStarButton = true;
+
+                RepositoryQueries queries = new();
+                var repo = await queries.GetOverview(Payload.Repository.Name.Split("/")[1], Payload.Repository.Name.Split("/")[0]);
+                RepoBlockViewModel.Item = repo;
+            }
+            else if (Payload.IsPushEvent)
+            {
+                CommitBlockViewModel = new();
+                CommitBlockViewModel.Ssh = Payload.PushEventPayload.Head;
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
