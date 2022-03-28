8d5decfa230c4df22e3fc565d6239a7654f34813
onein528 <onein528@outlook.com>
2022-03-29 00:58:32 +0900

    add a converter named StringNullOrEmptyToTrueConverter

---
+++ src/FluentHub/Converters/NullOrEmptyToVisibility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+#nullable enable
+
+namespace FluentHub.Converters
+{
+    public class StringNullOrEmptyToTrueConverter : ValueConverter<string, Visibility>
+    {
+        public bool Inverse { get; set; }
+
+        protected override Visibility Convert(string? value, object? parameter, string? language)
+        {
+            bool boolean = (Inverse ? !string.IsNullOrEmpty(value) : string.IsNullOrEmpty(value));
+
+            return boolean ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        protected override string ConvertBack(Visibility value, object? parameter, string? language)
+        {
+            return string.Empty;
+        }
+    }
+}
