a94f0c376d6dd3563756581ec346a773f2875ed9
onein528 <onein528@outlook.com>
2022-03-30 02:37:35 +0900

    add backend proj

---
+++ src/FluentHub.Backend/BackgroundTasks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace FluentHub.Backend
+{
+    public class BackgroundTasks : IBackgroundTask
+    {
+        public void Configure()
+        {
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+
+            deferral.Complete();
+        }
+    }
+}
