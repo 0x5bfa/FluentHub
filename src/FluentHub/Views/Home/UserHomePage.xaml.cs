e6236b6e64b8766a836dc4908ad4884acd12e627
onein528 <onein528@outlook.com>
2022-04-02 06:03:37 +0900

    urgent fix

--- src/FluentHub/Views/Home/UserHomePage.xaml.cs
+++ src/FluentHub/Views/Home/UserHomePage.xaml.cs
@@ -24,53 +24,54 @@ namespace FluentHub.Views.Home
                                         .FirstOrDefault(x => string.Compare(x.Tag.ToString(), e.Parameter?.ToString(), true) == 0)
                                         ?? defaultItem;
         }
+
         private void HomeNavView_SelectionChanged(muxc.NavigationView sender, muxc.NavigationViewSelectionChangedEventArgs args)
         {
             switch (args.SelectedItemContainer?.Tag?.ToString())
             {
                 case "profile":
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Collapsed;
-                    HomeContentFrame.Navigate(typeof(ProfilePage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(ProfilePage));
                     break;
                 case "notifications":
                     NavViewFrameTitleTextBlock.Text = "Your Notifications";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(NotificationsPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(NotificationsPage));
                     break;
                 case "activities":
                     NavViewFrameTitleTextBlock.Text = "Your Activities";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(ActivitiesPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(ActivitiesPage));
                     break;
                 case "issues":
                     NavViewFrameTitleTextBlock.Text = "Your Issues";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(IssuesPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(IssuesPage));
                     break;
                 case "pullrequests":
                     NavViewFrameTitleTextBlock.Text = "Your Pull Requests";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(PullRequestsPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(PullRequestsPage));
                     break;
                 case "discussions":
                     NavViewFrameTitleTextBlock.Text = "Your Discussions";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(DiscussionsPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(DiscussionsPage));
                     break;
                 case "repositories":
                     NavViewFrameTitleTextBlock.Text = "Your Repositories";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(RepositoriesPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(RepositoriesPage));
                     break;
                 case "organizations":
                     NavViewFrameTitleTextBlock.Text = "Your Organizations";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(OrganizationsPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(OrganizationsPage));
                     break;
                 case "starred":
                     NavViewFrameTitleTextBlock.Text = "Your Starred Repositories";
                     NavViewFrameTitleTextBlock.Visibility = Visibility.Visible;
-                    HomeContentFrame.Navigate(typeof(StarredReposPage), App.SignedInUserName);
+                    HomeContentFrame.Navigate(typeof(StarredReposPage));
                     break;
             }
         }
