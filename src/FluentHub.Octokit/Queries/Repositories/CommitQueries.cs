e4ac7ab2fa7c7bd964814b559f16ea674e989bbc
onein528 <onein528@outlook.com>
2022-04-13 20:47:07 +0900

    Revert "CodeFactor"
    This reverts commit f6b4edc5f6f0156f0d24e5e50e2bfab4356c1fc8.

--- src/FluentHub.Octokit/Queries/Repositories/CommitQueries.cs
+++ src/FluentHub.Octokit/Queries/Repositories/CommitQueries.cs
@@ -183,6 +183,7 @@ namespace FluentHub.Octokit.Queries.Repositories
                     CommittedAtHumanized = res2.CommittedDate.Humanize(),
                 };
 
+
                 items.Add(item);
             }
             #endregion
