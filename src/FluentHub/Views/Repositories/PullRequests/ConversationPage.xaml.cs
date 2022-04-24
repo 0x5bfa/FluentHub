4b2f23071b3d2e661a206d223441901cd08b0214
onein528 <onein528@outlook.com>
2022-04-24 13:30:32 +0900

    updates

--- src/FluentHub/Views/Repositories/PullRequests/ConversationPage.xaml.cs
+++ src/FluentHub/Views/Repositories/PullRequests/ConversationPage.xaml.cs
@@ -1,30 +1,31 @@
 ﻿using System;
+using FluentHub.Services;
+using FluentHub.ViewModels.Repositories.PullRequests;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+using muxc = Microsoft.UI.Xaml.Controls;
 
 namespace FluentHub.Views.Repositories.PullRequests
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
     public sealed partial class ConversationPage : Page
     {
         public ConversationPage()
         {
             this.InitializeComponent();
+
+            var provider = App.Current.Services;
+            ViewModel = provider.GetRequiredService<ConversationViewModel>();
+            navigationService = provider.GetRequiredService<INavigationService>();
         }
+
+        private readonly INavigationService navigationService;
+        public ConversationViewModel ViewModel { get; }
     }
 }
