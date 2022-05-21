d156167845500f3105a6b5e62390d895325ba648
onein528 <onein528@outlook.com>
2022-05-21 16:52:05 +0900

    fixed

--- src/FluentHub.Octokit/Models/Events/IssueComment.cs
+++ src/FluentHub.Octokit/Models/Events/IssueComment.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace FluentHub.Octokit.Models.Events
 {
     public class IssueComment
