c57ce9a62dc4f295164f743c7c488f21a6424dcb
onein528 <onein528@outlook.com>
2022-05-26 22:22:07 +0900

    Added emoji table and remaned Backend to Core

---
+++ src/FluentHub.Core/Emoji/Emoji.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentHub.Backend.Emoji
+{
+    public sealed class Emoji
+    {
+        public string Name { get; set; }
+        public string EmojiUrl { get; set; }
+        public string Value { get; set; }
+        public bool Deprecated { get; set; }
+
+        public static Emoji Define(string value, string name, bool deprecated = false)
+        {
+            return new Emoji
+            {
+                Name = name,
+                Value = value,
+                Deprecated = deprecated,
+            };
+        }
+    }
+}
--- src/FluentHub/Helpers/WebViewHelpers.cs
+++ src/FluentHub/Helpers/WebViewHelpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace FluentHub.Helpers
