e87948b081de82df0c18a9ffd55dd33967d14f83
onein528 <onein528@outlook.com>
2022-05-27 00:53:25 +0900

    Added mapping dir

---
+++ src/FluentHub.Octokit/Mapping/NotificationMapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentHub.Octokit.Mapping
+{
+    public class NotificationMapping
+    {
+        public static List<Notification> MapAll(IEnumerable<OctokitOriginal.Notification> data)
+        {
+            var result = new List<Notification>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    result.Add(Map(item));
+                }
+            }
+
+            return result;
+        }
+
+        public static Notification Map(OctokitOriginal.Notification data)
+        {
+            return new Notification()
+            {
+            };
+        }
+    }
+}
