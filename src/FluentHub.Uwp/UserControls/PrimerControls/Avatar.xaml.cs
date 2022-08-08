83306bf93587fe78bffc08c0fb60859d0e7ddb98
U+5BFA <onein528@outlook.com>
2022-08-09 00:20:26 +0900

    Updated all issue event data templates

--- src/FluentHub.Uwp/UserControls/PrimerControls/Avatar.xaml.cs
+++ src/FluentHub.Uwp/UserControls/PrimerControls/Avatar.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
+﻿using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -13,15 +8,85 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// ユーザー コントロールの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234236 を参照してください
-
 namespace FluentHub.Uwp.UserControls.PrimerControls
 {
+    /// <summary>
+    /// See https://primer.style/react/Avatar
+    /// </summary>
     public sealed partial class Avatar : UserControl
     {
-        public Avatar()
+        #region propdp
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(
+                nameof(Source),
+                typeof(string),
+                typeof(Avatar),
+                new PropertyMetadata(null)
+                );
+
+        public string Source
         {
-            this.InitializeComponent();
+            get => (string)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
         }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(Avatar),
+                new PropertyMetadata(null)
+                );
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register(
+                nameof(Size),
+                typeof(double),
+                typeof(Avatar),
+                new PropertyMetadata(20D)
+                );
+
+        public double Size
+        {
+            get => (double)GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+
+        public static readonly DependencyProperty IsSquareProperty =
+            DependencyProperty.Register(
+                nameof(IsSquare),
+                typeof(bool),
+                typeof(Avatar),
+                new PropertyMetadata(false)
+                );
+
+        public bool IsSquare
+        {
+            get => (bool)GetValue(IsSquareProperty);
+            set => SetValue(IsSquareProperty, value);
+        }
+
+        public static readonly DependencyProperty IsClickableProperty =
+            DependencyProperty.Register(
+                nameof(IsClickable),
+                typeof(bool),
+                typeof(Avatar),
+                new PropertyMetadata(false)
+                );
+
+        public bool IsClickable
+        {
+            get => (bool)GetValue(IsClickableProperty);
+            set => SetValue(IsClickableProperty, value);
+        }
+        #endregion
+
+        public Avatar() => InitializeComponent();
     }
 }
--- src/FluentHub.Uwp/UserControls/PrimerControls/Timeline.xaml.cs
+++ src/FluentHub.Uwp/UserControls/PrimerControls/Timeline.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
+﻿using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
