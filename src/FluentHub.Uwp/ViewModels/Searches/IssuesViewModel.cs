d25e3b43c6ec593d70c94830a25505aada4c30d6
U+5BFA <onein528@outlook.com>
2022-09-25 01:23:27 +0900

    Added some search result Pages and those ViewModels

---
+++ src/FluentHub.Uwp/ViewModels/Searches/IssuesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using FluentHub.Octokit.Searches;
+using FluentHub.Uwp.Helpers;
+using FluentHub.Uwp.Models;
+using FluentHub.Uwp.Services;
+using FluentHub.Uwp.ViewModels.Repositories;
+using FluentHub.Uwp.ViewModels.UserControls;
+using FluentHub.Uwp.ViewModels.UserControls.Overview;
+using FluentHub.Uwp.ViewModels.UserControls.BlockButtons;
+using FluentHub.Uwp.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Media.Imaging;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+namespace FluentHub.Uwp.ViewModels.Searches
+{
+    public class IssuesViewModel : ObservableObject
+    {
+        public IssuesViewModel(IMessenger messenger = null, ILogger logger = null)
+        {
+            _messenger = messenger;
+            _logger = logger;
+        }
+
+        #region Fields and Properties
+        private readonly ILogger _logger;
+        private readonly IMessenger _messenger;
+        #endregion
+    }
+}
