43e1c0ede61ded2b8912b76709d73ace0072d02e
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-08-09 00:49:51 +0900

    Fix: Fixed an issue where the emoji converter could cause an exception

--- src/FluentHub.App/Converters/EmojiShortCodeToUnicodeConverter.cs
+++ src/FluentHub.App/Converters/EmojiShortCodeToUnicodeConverter.cs
@@ -2,16 +2,19 @@ using Microsoft.UI.Xaml.Data;
 
 namespace FluentHub.App.Converters
 {
-    public class EmojiShortCodeToUnicodeConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            var shortCode = value.ToString();
+	public class EmojiShortCodeToUnicodeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var shortCode = value?.ToString();
 
-            return Core.Extensions.Emoji.EmojiMapping.GetUnicode(shortCode);
-        }
+			if (string.IsNullOrEmpty(shortCode))
+				return null;
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-            => throw new NotImplementedException();
-    }
+			return Core.Extensions.Emoji.EmojiMapping.GetUnicode(shortCode);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+			=> throw new NotImplementedException();
+	}
 }
