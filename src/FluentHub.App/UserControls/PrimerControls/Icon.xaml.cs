2d590d8974cefe1204abc143c53eb86dc28ca5d0
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-08-13 10:54:03 +0900

    Feature: Introduced generator for Primer Octicon

---
+++ src/FluentHub.App/UserControls/PrimerControls/Icon.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace FluentHub.App.UserControls.PrimerControls
+{
+	public sealed partial class Icon : Control
+	{
+		public Icon()
+		{
+			InitializeComponent();
+		}
+	}
+}
