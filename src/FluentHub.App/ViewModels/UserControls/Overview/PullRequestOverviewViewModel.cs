f5ca97ffee35c8132dcb998f429d7f3d6f0d57a6
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-03-11 00:03:04 +0900

    Add shimmer

--- src/FluentHub.App/ViewModels/UserControls/Overview/PullRequestOverviewViewModel.cs
+++ src/FluentHub.App/ViewModels/UserControls/Overview/PullRequestOverviewViewModel.cs
@@ -5,12 +5,10 @@ namespace FluentHub.App.ViewModels.UserControls.Overview
 {
 	public class PullRequestOverviewViewModel : ObservableObject
 	{
-		#region Fields and Properties
 		private PullRequest _pullRequest;
 		public PullRequest PullRequest { get => _pullRequest; set => SetProperty(ref _pullRequest, value); }
 
 		private string _selectedTag;
 		public string SelectedTag { get => _selectedTag; set => SetProperty(ref _selectedTag, value); }
-		#endregion
 	}
 }
