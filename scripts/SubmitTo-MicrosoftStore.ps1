755400ec30b0ecf60f39aeb420df3382741e8428
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-07-25 13:31:20 +0900

    Update SubmitTo-MicrosoftStore.ps1

--- scripts/SubmitTo-MicrosoftStore.ps1
+++ scripts/SubmitTo-MicrosoftStore.ps1
@@ -11,18 +11,19 @@ param(
     [string]$PartnerCenterTenantId = ""
 )
 
+# Setup environment
 Set-ExecutionPolicy RemoteSigned -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module -Name StoreBroker
 
 # Authenticate StoreBroker
-$username = $env:PartnerCenterClientId
-$password = ConvertTo-SecureString $env:PartnerCenterClientSecret -AsPlainText -Force
+$username = $PartnerCenterClientId
+$password = ConvertTo-SecureString $PartnerCenterClientSecret -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($username, $password)
 Set-StoreBrokerAuthentication -TenantId $PartnerCenterTenantId -Credential $cred
 
 # Prepare the submission package
-New-SubmissionPackage -ConfigPath $StoreBrokerConfigPath -AppxPath $appxUploadFilePath -OutPath $SubmissionDirPath -OutName 'submission'
+New-SubmissionPackage -ConfigPath $StoreBrokerConfigPath -AppxPath $AppxPackagePath -OutPath $SubmissionDirPath -OutName 'submission'
 $SubmissionDataPath = Join-Path -Path $SubmissionDirPath -ChildPath 'submission.json'
 $SubmissionPackagePath = Join-Path -Path $SubmissionDirPath -ChildPath 'submission.zip'
 
