d04f6bca41cac6416b6291fa207678de814b61fd
Lamparter <notlamparter@outlook.com>
2024-09-05 08:19:53 +0100

    ⏪️ Revert changes to organised import statements

--- src/FluentHub.Octokit/GlobalUsings.cs
+++ src/FluentHub.Octokit/GlobalUsings.cs
@@ -1,19 +1,23 @@
 ﻿// Copyright (c) 2022-2024 0x5BFA
 // Licensed under the MIT License. See the LICENSE.
 
+// Default usings
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.Linq;
+global using global::System.Linq.Expressions;
+global using global::System.Runtime.Serialization;
+global using global::System.Text;
+global using global::System.Threading.Tasks;
+global using global::Newtonsoft.Json;
+global using global::Newtonsoft.Json.Converters;
+
+// Custom usings
 global using global::FluentHub.Octokit.Models.v3;
 global using global::FluentHub.Octokit.Models.v4;
 global using global::FluentHub.Octokit.Models.v4Custom;
 global using global::Humanizer;
-global using global::Newtonsoft.Json;
-global using global::Newtonsoft.Json.Converters;
 global using global::Octokit.GraphQL;
-global using global::System;
-global using global::System.Collections.Generic;
-global using global::System.Linq;
-global using global::System.Runtime.Serialization;
-global using global::System.Text;
-global using global::System.Threading.Tasks;
+global using OctokitV3 = global::Octokit;
 global using OctokitGraphQLCore = global::Octokit.GraphQL.Core;
 global using OctokitGraphQLModel = global::Octokit.GraphQL.Model;
-global using OctokitV3 = global::Octokit;
